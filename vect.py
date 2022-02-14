62ba21e4afbed29b78b18fc17e55b23e21f41604
Arthur Langlard <arthur.electron@orange.fr>
2022-02-14 17:50:14 +0100

    Adding copyright notices.

--- vect.py
+++ vect.py
@@ -1,19 +1,20 @@
 # ##### BEGIN GPL LICENCE BLOCK #####
+#  Copyright (C) 2022  Arthur Langlard
 #
-# This file is part of Atomic Blender (CIF).
+#  This file is part of Atomic Blender (CIF).
 #
-# Atomic Blender (CIF) is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3
-# of the License, or (at your option) any later version.
+#  Atomic Blender (CIF) is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3
+#  of the License, or (at your option) any later version.
 #
-# Atomic Blender (CIF) is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+#  Atomic Blender (CIF) is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with Foobar. If not, see <https://www.gnu.org/licenses/>
+#  You should have received a copy of the GNU General Public License
+#  along with Foobar. If not, see <https://www.gnu.org/licenses/>
 #
 # ##### END GPL LICENCE BLOCK #####
 
